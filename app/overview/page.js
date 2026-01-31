6a750537c889102cbcc619dd91d975a374ec300c
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-01-31 15:53:17 +0200

    hide scroll on y

--- app/overview/page.js
+++ app/overview/page.js
@@ -8,7 +8,7 @@ import React from "react";
 
 export default function OverviewPage() {
   return (
-    <div className="flex-1 overflow-auto relative z-10  ">
+    <div className="flex-1 overflow-hidden relative z-10  ">
       <main className="max-w-7xl mx-auto py-4 px-4 lg:px-8 ">
         <motion.div
           className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8 "
