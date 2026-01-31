cdc40ff56c8b6b032bf17a81cf3e00772df9a876
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-01-31 13:10:18 +0200

    nav

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+import admin from "../public/images/admin.png"
+import uk from "../public/images/uk.png"
+import { Bell } from 'lucide-react';
+
+
+export default function Header() {
+    return (
+        <header className='bg-[#1e1e1e] shadow-lg border-b border-[#1f1f1f] mx-4 sm:mx-6 lg:mx-8 mt-4 mb-2 rounded-lg '>
+            <div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 flex items-center justify-between  '>
+                <h1 className='capitalize text-lg sm:text-xl lg:text-2xl font-semibold text-gray-100'>dashboard</h1>
+
+                <div className='flex items-center space-x-3 sm:space-x-6'>
+                    <Image src={uk} alt="country flag" width={25} height={18} className="rounded-full shadow-md cursor-pointer" />
+                    <div className='relative '>
+                        <Bell className='w-5 sm:w-6 h-5 sm:h-6 text-gray-300 cursor-pointer hover:text-white ' />
+                    </div>
+                    <div className='flex items-center space-x-2 sm:space-x-3 '>
+                        <Image src={admin} alt='admin' width={35} height={35} className='rounded-full  border border-gray-600' />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
