005e9b649aa3016b64c32967bbf7123e82c18baa
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-02-02 16:22:01 +0200

    $

--- app/overview/page.js
+++ app/overview/page.js
@@ -18,7 +18,7 @@ export default function OverviewPage() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
-          <StatCard name="Total Sales" icon={DollarSign} value="182,450 EGP" />
+          <StatCard name="Total Sales" icon={DollarSign} value="$182,450" />
           <StatCard name="Total Clients" icon={User} value="1,437" />
           <StatCard name="Total Products" icon={ShoppingBag} value="674" />
           <StatCard name="Stock" icon={SquareActivity} value="12,845" />
